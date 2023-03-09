39e02eade3f420113c085676954ffc7730570e8f
Scot Nicol <scnicol@ucdavis.edu>
2023-03-08 16:18:49 -0800

    added Users table migration, seeds, constraints and validations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,24 +1,51 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       // define association here
     }
-  }
+  };
+
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.")
+          }
+        }
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    },
   }, {
     sequelize,
     modelName: 'User',
---
+++ backend/db/seeders/20230309001052-demo-user.js
@@ -0,0 +1,44 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
+      {
+        firstName: 'Demo',
+        lastName: 'Man',
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        firstName: 'Fake',
+        lastName: 'Man',
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        firstName: 'Real',
+        lastName: 'Man',
+        email: 'user2@user.io',
+        username: 'RealUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'RealUser2'] }
+    }, {});
+  }
+};
