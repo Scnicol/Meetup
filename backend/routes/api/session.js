13f90e1527f1a31d13467dcc0f0ddcd7834b41ee
Scot Nicol <scnicol@ucdavis.edu>
2023-03-08 17:28:41 -0800

    Added User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -27,5 +27,12 @@ router.post(
     }
   );
 
+  // Log out
+router.delete('/', (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 
 module.exports = router;
