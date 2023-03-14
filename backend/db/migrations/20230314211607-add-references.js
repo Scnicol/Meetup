0165d502df58b6c8b09397eb96036043eb72f848
Scot Nicol <scnicol@ucdavis.edu>
2023-03-14 16:06:09 -0700

    added options to the add-references migration file

--- backend/db/migrations/20230314211607-add-references.js
+++ backend/db/migrations/20230314211607-add-references.js
@@ -1,67 +1,82 @@
 'use strict';
 
+let options = {};
+if (ProcessingInstruction.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     //adding the organizerId to the Groups table
-    await queryInterface.addColumn('Groups', 'organizerId', {
+    options.tableName = "Groups";
+    await queryInterface.addColumn(options, 'organizerId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Users'},
     });
     //adding the userId to the Memberships table
-    await queryInterface.addColumn('Memberships', 'userId', {
+    options.tableName = "Memberships";
+    await queryInterface.addColumn(options, 'userId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Users'},
       onDelete: 'cascade',
     });
     //adding the groupId to the Memberships table
-    await queryInterface.addColumn('Memberships', 'groupId', {
+    options.tableName = "Memberships";
+    await queryInterface.addColumn(options, 'groupId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Groups'},
       onDelete: 'cascade',
     });
     //adding the groupId to the Venues table
-    await queryInterface.addColumn('Venues', 'groupId', {
+    options.tableName = "Venues";
+    await queryInterface.addColumn(options, 'groupId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Groups'},
     });
     //adding the groupId to the Events table
-    await queryInterface.addColumn('Events', 'groupId', {
+    options.tableName = "Events";
+    await queryInterface.addColumn(options, 'groupId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Groups'},
     });
     //adding the venueId to the Events table
-    await queryInterface.addColumn('Events', 'venueId', {
+    options.tableName = "Events";
+    await queryInterface.addColumn(options, 'venueId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Venues'},
     });
     //adding the eventId to the EventImages table
-    await queryInterface.addColumn('EventImages', 'eventId', {
+    options.tableName = "EventImages";
+    await queryInterface.addColumn(options, 'eventId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Events'},
     });
     //adding userId to the Attendances table
-    await queryInterface.addColumn('Attendances', 'userId', {
+    options.tableName = "Attendances";
+    await queryInterface.addColumn(options, 'userId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Users'},
       onDelete: 'cascade',
     });
     //adding eventId to the Attendances table
-    await queryInterface.addColumn('Attendances', 'eventId', {
+    options.tableName = "Attendances";
+    await queryInterface.addColumn(options, 'eventId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Events'},
       onDelete: 'cascade',
     });
     //adding groupId to the GroupImages table
-    await queryInterface.addColumn('GroupImages', 'groupId', {
+    options.tableName = "GroupImages";
+    await queryInterface.addColumn(options, 'groupId', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {model: 'Groups'},
@@ -69,15 +84,25 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Groups', 'organizerId');
-    await queryInterface.removeColumn('Memberships', 'userId');
-    await queryInterface.removeColumn('Memberships', 'groupId');
-    await queryInterface.removeColumn('Venues', 'groupId');
-    await queryInterface.removeColumn('Events', 'groupId');
-    await queryInterface.removeColumn('Events', 'venueId');
-    await queryInterface.removeColumn('EventImages', 'eventId');
-    await queryInterface.removeColumn('Attendances', 'userId');
-    await queryInterface.removeColumn('Attendances', 'eventId');
-    await queryInterface.removeColumn('GroupImages', 'groupId');
+    options.tableName = "Groups";
+    await queryInterface.removeColumn(options, 'organizerId');
+    options.tableName = "Memberships";
+    await queryInterface.removeColumn(options, 'userId');
+    options.tableName = "Memberships";
+    await queryInterface.removeColumn(options, 'groupId');
+    options.tableName = "Venues'";
+    await queryInterface.removeColumn(options, 'groupId');
+    options.tableName = "Events";
+    await queryInterface.removeColumn(options, 'groupId');
+    options.tableName = "Events";
+    await queryInterface.removeColumn(options, 'venueId');
+    options.tableName = "EventImages";
+    await queryInterface.removeColumn(options, 'eventId');
+    options.tableName = "Attendances";
+    await queryInterface.removeColumn(options, 'userId');
+    options.tableName = "Attendances";
+    await queryInterface.removeColumn(options, 'eventId');
+    options.tableName = "GroupImages";
+    await queryInterface.removeColumn(options, 'groupId');
   }
 };
