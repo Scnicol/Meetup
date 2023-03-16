fc6809125a0ba0e0160d729f064829bc9e88e665
Scot Nicol <scnicol@ucdavis.edu>
2023-03-16 12:54:16 -0700

    added backend API endpoints for /groups

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Group, Membership, User, GroupImage } = require('../../db/models');
+const { Group, User, GroupImage } = require('../../db/models');
 const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const sequelize = require('sequelize');
 const { Op } = require("sequelize");
+const group = require('../../db/models/group');
+
+//Add an Image to a Group based on the Group's id
+router.post('/:groupId/images', requireAuth, async (req, res, next) => {
+    let groupId = parseInt(req.params.groupId);
+    const {url, preview} = req.body;
+
+    const currGroup = await Group.findByPk(groupId);
+
+    if (!currGroup) {
+        const err = new Error("Group couldn't be found");
+        err.status = 404;
+        return next(err)
+    }
+
+    const newImage = await GroupImage.create({
+        groupId,
+        url,
+        preview,
+    });
+
+    res.json({ message: "Success"})
+});
 
 //Create a Group
 router.post('/', requireAuth, async (req, res, next) => {
@@ -28,6 +51,68 @@ router.post('/', requireAuth, async (req, res, next) => {
     res.status(201).json(newGroup);
 });
 
+//Edit a Group, updates and returns an existing group
+router.put('/:groupId', requireAuth, async (req, res, next) => {
+    const groupId = parseInt(req.params.groupId);
+
+    const {name, about, type, private, city, state} = req.body;
+
+    let group = await Group.findByPk(groupId);
+
+    if (!group) {
+        const err = new Error("Group couldn't be found");
+        err.status = 404;
+        return next(err)
+    }
+    group.set({
+        name,
+        about,
+        type,
+        private,
+        city,
+        state,
+    })
+
+    res.json(group);
+});
+
+//Get all Groups by the Current User
+router.get('/current', requireAuth, async (req, res, next) => {
+    const currUserId = req.user.id;
+
+    const currUserGroups = await User.findByPk(currUserId, {
+        attributes: [],
+        include: [
+            {model: Group, as: "Groups"}
+        ]
+    })
+    res.json(currUserGroups);
+});
+
+//Get details of a Group specified by its id
+router.get('/:groupId', async (req, res, next) => {
+    const groupId = parseInt(req.params.groupId);
+
+    const currGroup =await Group.findByPk(groupId, {
+        attributes: {
+        },
+        include: [
+            { model: User },
+            {
+                model: GroupImage, as: "GroupImages",
+                attributes: ['id', 'url', 'preview'],
+            },
+        ]
+    });
+
+    // should the user be considered a member or not? This will change how I create groups
+    currGroup.dataValues.numMembers = currGroup.Users.length;
+    delete currGroup.dataValues.Users
+
+
+    res.json(currGroup);
+});
+
 //Get all groups, include: numMembers, preview
 router.get('/', async (req, res, next) => {
     const groups = await Group.findAll({
@@ -43,12 +128,8 @@ router.get('/', async (req, res, next) => {
     });
 
     for (let i = 0; i < groups.length; i++) {
-        console.log(groups[i].Users.length);
         groups[i].dataValues.numMembers = groups[i].Users.length;
         delete groups[i].dataValues.Users
-
-        // groups[i].dataValues.GroupImages = groups[i].GroupImage;
-        // delete groups[i].dataValues.GroupImage;
     }
     console.log(groups)
 
