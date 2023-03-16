91f3fe85c5b0435c061b05a2b4f33a2d76b5f24a
Scot Nicol <scnicol@ucdavis.edu>
2023-03-16 16:22:35 -0700

    added Edit a Venue specified by its id

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -6,5 +6,28 @@ const router = express.Router();
 
 //Edit a Venue specified by its id
 router.put('/:venueId', requireAuth, async (req, res, next) => {
-    
+    const venueId = parseInt(req.params.venueId);
+    const { address, city, state, lat, lng} = req.body;
+
+    let venue = await Venue.findByPk(venueId, {
+        attributes: ['id', 'groupId', 'address', 'city', 'state', 'lat', 'lng']
+    });
+
+    if (!venue) {
+        const err = new Error("Venue couldn't be found");
+        err.status = 404;
+        return next(err)
+    }
+
+    venue.set({
+        address,
+        city,
+        state,
+        lat,
+        lng
+    });
+
+    res.json(venue);
 });
+
+module.exports = router;
