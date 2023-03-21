7a74f7549ca7713f00f93866392fb7a6e4a09843
Scot Nicol <scnicol@ucdavis.edu>
2023-03-21 13:49:51 -0700

    changed the log in response object for postman specs to work

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,9 +32,9 @@ router.post('/', validateLogin, async (req, res, next) => {
 
       await setTokenCookie(res, user);
 
-      return res.json({
-        user: user
-      });
+      return res.json(
+        user
+      );
     }
   );
 
