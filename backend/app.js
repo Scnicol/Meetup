ed5aeb203a555bed86dcf307f75eaddfcbee5bdd
Scot Nicol <scnicol@ucdavis.edu>
2023-03-23 15:36:27 -0700

    modified status code with a default of 400

--- backend/app.js
+++ backend/app.js
@@ -77,7 +77,7 @@ app.use((err, _req, res, _next) => {
     res.json({
       // title: err.title || 'Server Error',
       message: err.message,
-      statusCode: err.status,
+      statusCode: err.status || 400,
       errors: err.errors,
       // stack: isProduction ? null : err.stack
     });
