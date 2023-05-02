a46fb4a5dba370d01db1f39c1e47951e6e167e02
Scot Nicol <scnicol@ucdavis.edu>
2023-05-02 15:11:18 -0700

    removed old code from updateGroupForm to instead be used in GroupForm

--- frontend/src/components/Groups/updateGroupForm.js
+++ frontend/src/components/Groups/updateGroupForm.js
@@ -24,42 +24,6 @@ function UpdateGroupForm({ hideForm }) {
         console.log(newGroup, 'newgroup inside submitAction')
         return updateGroup(newGroup);
     }
-    // const dispatch = useDispatch();
-    // const history = useHistory();
-    // const [name, setName] = useState('');
-    // const [about, setAbout] = useState('');
-    // const [type, setType] = useState("In person");
-    // const [isPrivate, setIsPrivate] = useState(true);
-    // const [city, setCity] = useState('');
-    // const [state, setState] = useState('');
-
-    // const updateName = (e) => setName(e.target.value);
-    // const updateAbout = (e) => setAbout(e.target.value);
-    // const updateType = (e) => setType(e.target.value);
-    // const updateIsPrivate = (e) => setIsPrivate(e.target.value);
-    // const updateCity = (e) => setCity(e.target.value);
-    // const updateState = (e) => setState(e.taget.value);
-
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-
-    //     const payload = {
-    //         name,
-    //         about,
-    //         type,
-    //         isPrivate,
-    //         city,
-    //         state,
-    //     };
-
-    //     let updatedGroup;
-    //     updatedGroup = await dispatch(updateGroup(payload));
-
-    //     if (updatedGroup) {
-    //         history.push(`/api/groups/${updatedGroup.id}`);
-    //         hideForm();
-    //     }
-    // };
 
     return (
         <GroupForm group={group} formType="Update" submitAction={submitAction} hideForm={hideForm}/>
